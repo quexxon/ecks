18d40306a396217cde0c1a38f005c7780ec4a910
Will Clardy <will@quexxon.net>
2021-10-15 08:27:31 -0400

    Make REPL more friendly (sloppy)
    - line continuations
    - color by type
    - print strings with appropriate quotes

--- src/repl.ts
+++ src/repl.ts
@@ -1,30 +1,80 @@
-import * as readline from 'readline'
+import * as ReadLine from 'readline'
+import * as Chalk from 'chalk'
 // import { inspect } from 'util'
 import Interpreter from './interpreter'
 
 import Parser from './parser'
 import Scanner from './scanner'
+import XString from './std/string'
+import XInteger from './std/integer'
+import XFloat from './std/float'
+import XBoolean from './std/boolean'
 
-const rl = readline.createInterface({
+const rl = ReadLine.createInterface({
   input: process.stdin,
   output: process.stdout,
-  prompt: '> '
+  terminal: true,
+  prompt: '> ',
+  historySize: 1000,
+  removeHistoryDuplicates: true,
+  tabSize: 2
 })
 
 rl.prompt()
 
+let input = ''
+
 rl.on('line', (line) => {
-  const start = process.hrtime.bigint()
-  const scanner = new Scanner(line)
-  const parser = new Parser(scanner.scan())
-  const interpreter = new Interpreter(parser.parse())
-  const value = interpreter.eval()
-  const duration = process.hrtime.bigint() - start
+  input += line
 
-  console.log(`Elapsed: ${Number(duration) / 1e6}ms`)
-  console.log(value.__toString(), `(${value.kind})`)
+  if (line[line.length - 1] === ' ') {
+    rl.setPrompt('| ')
+    rl.prompt()
+    return
+  }
 
+  try {
+    const start = process.hrtime.bigint()
+    const scanner = new Scanner(input)
+    const parser = new Parser(scanner.scan())
+    const interpreter = new Interpreter(parser.parse())
+    const value = interpreter.eval()
+    const duration = process.hrtime.bigint() - start
+
+    let printValue: string = value.__toString()
+    if (value instanceof XString) {
+      if (value.__value.includes("'")) {
+        printValue = `"${Chalk.green(value.__value)}"`
+      } else {
+        printValue = `'${Chalk.green(value.__value)}'`
+      }
+    } else if (value instanceof XInteger || value instanceof XFloat) {
+      printValue = Chalk.yellow(printValue)
+    } else if (value instanceof XBoolean) {
+      printValue = Chalk.magenta(printValue)
+    }
+
+    console.log(`${printValue} (${value.kind})`)
+    if (process.env.DEBUG === '1') {
+      console.log(`Elapsed: ${Number(duration) / 1e6}ms`)
+    }
+  } catch (err) {
+    if (!(err instanceof SyntaxError || err instanceof TypeError) && line.length > 0) {
+      rl.setPrompt('| ')
+      rl.prompt()
+      return
+    } else if (err instanceof Error) {
+      console.log(`${err.name}: ${err.message}`)
+    } else {
+      throw new Error('Unexpected Error')
+    }
+  }
+
+  input = ''
+  rl.setPrompt('> ')
   rl.prompt()
-}).on('close', () => {
+})
+
+rl.on('close', () => {
   process.exit(0)
 })
