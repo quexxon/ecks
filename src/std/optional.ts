febac78dac782c2e83685d50684fdb5da8d8258c
Will Clardy <will@quexxon.net>
2021-10-20 00:02:25 -0400

    Add map method to XOptional

--- src/std/optional.ts
+++ src/std/optional.ts
@@ -1,5 +1,6 @@
 import { TypedValue } from '../ast'
 import { State } from '../types'
+import XLambda from './lambda'
 
 export default class XOptional {
   kind = 'optional'
@@ -13,6 +14,16 @@ export default class XOptional {
 
   [Symbol.for('??')] (value: TypedValue): TypedValue {
     return this.#value === undefined ? value : this.#value
+  }
+
+  map (fn: TypedValue): XOptional {
+    if (!(fn instanceof XLambda)) throw new TypeError('Expected a lambda')
+    if (fn.__value.params.length !== 1) {
+      throw new TypeError('Expected a single parameter')
+    }
+
+    if (this.#value === undefined) return this.__new()
+    return this.__new(fn.call(this.#value))
   }
 
   get __value (): TypedValue | undefined { return this.#value }
